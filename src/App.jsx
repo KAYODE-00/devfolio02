375e79f86ec04607b0e8e566a2c3fadc0c21d701
Abdulwahab Kayode <dahbiigkayode00@gmail.com>
2026-03-01 16:28:53 +0100

    service summsry section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Navbar from './Sections/Navbar'
 import Hero from './Sections/Hero'
+import ServiceSummary from "./sections/ServiceSummary";
+
 
 const App = () => {
   return (
     <div className='relative w-screen min-h-screen overflow-x-auto'>
-      <Navbar/>
-      <Hero/>
+<Navbar />
+        <Hero />
+        <ServiceSummary />
+        
+        {/* <Services />
+        <About />
+        <Works />
+        <ContactSummary />
+        <Contact /> */}
     </div>
   )
 }
