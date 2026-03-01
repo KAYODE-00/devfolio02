4c084cefd81311a2b50cd07a7c15906e48066a81
Abdulwahab Kayode <dahbiigkayode00@gmail.com>
2026-03-01 16:18:27 +0100

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from './Sections/Navbar'
+import Hero from './Sections/Hero'
 
 const App = () => {
   return (
     <div className='relative w-screen min-h-screen overflow-x-auto'>
       <Navbar/>
-      <section  id='Home' className='min-h-screen'/>
-       <section id='Services' className='min-h-screen'/>
+      <Hero/>
     </div>
   )
 }
